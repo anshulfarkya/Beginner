b80829df373e199326101f422a5adae644491b6f
anshulfarkya <39838159+anshulfarkya@users.noreply.github.com>
2021-07-30 15:33:49 +0530

    Creating a new branch and adding comments to Add.cpp

--- Add.cpp
+++ Add.cpp
@@ -1,8 +1,10 @@
+//importing header files
 #include<iostream.h>
 #include<conio.h>
 using namespace std;
 int main()
 {
+  //declaring variables
   int a, b;
   int sum = a+b;
   cout<<"sum = "<<sum;
